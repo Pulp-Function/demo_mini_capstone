7b5d7f10a5a465e94bd26da75db94306978c6dd0
Peter Jang <peter.x.jang@gmail.com>
2021-01-14 20:39:21 -0600

    Add products RESTful CRUD actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,21 +1,38 @@
 class Api::ProductsController < ApplicationController
-  def all_products_method
+  def index
     @products = Product.all
-    render "all_products.json.jb"
+    render "index.json.jb"
   end
 
-  def first_product_method
-    @product = Product.first
-    render "one_product.json.jb"
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    @product.save
+    render "show.json.jb"
   end
 
-  def second_product_method
-    @product = Product.second
-    render "one_product.json.jb"
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
   end
 
-  def third_product_method
-    @product = Product.third
-    render "one_product.json.jb"
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: { message: "Product destroyed successfully!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/all_products_path" => "products#all_products_method"
-    get "/first_product_path" => "products#first_product_method"
-    get "/second_product_path" => "products#second_product_method"
-    get "/third_product_path" => "products#third_product_method"
+    get "/products" => "products#index"
+    post "/products" => "products#create"
+    get "/products/:id" => "products#show"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
   end
 end
