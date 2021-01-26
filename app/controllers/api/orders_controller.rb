27ba2335d98e634b76f57772d9c1ccb72e78b00a
Peter Jang <peter.x.jang@gmail.com>
2021-01-25 20:57:41 -0600

    Add orders create action

---
+++ app/controllers/api/orders_controller.rb
@@ -0,0 +1,23 @@
+class Api::OrdersController < ApplicationController
+  def create
+    # Open the products table
+    # Find the product with an id that matches the product_id given by the user
+    product = Product.find_by(id: params[:product_id])
+    # Get the price from the product and multiply it by the quantity given by the user
+    calculated_subtotal = product.price * params[:quantity].to_i
+    # Calculate tax and total
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    # Save the order in the database
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
+    )
+    @order.save
+    render "show.json.jb"
+  end
+end
