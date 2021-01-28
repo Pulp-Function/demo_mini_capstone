cde344d3bb12c498fa5c7b32bca9f2d598078718
Peter Jang <peter.x.jang@gmail.com>
2021-01-27 18:18:00 -0600

    Fix incorrect association

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,7 +19,7 @@ class Product < ApplicationRecord
   has_many :orders
 
   has_many :category_products
-  has_many :products, through: :category_products
+  has_many :categories, through: :category_products
 
   scope :title_search, ->(search_terms) do
           if search_terms
