eb808177414fb4ebfb1f920407e410030ca7cb8b
Peter Jang <peter.x.jang@gmail.com>
2021-01-28 21:02:23 -0600

    Update the orders create action

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,24 +7,23 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    # Open the products table
-    # Find the product with an id that matches the product_id given by the user
-    product = Product.find_by(id: params[:product_id])
-    # Get the price from the product and multiply it by the quantity given by the user
-    calculated_subtotal = product.price * params[:quantity].to_i
-    # Calculate tax and total
+    carted_products = current_user.carted_products.where(status: "carted")
+
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
     calculated_tax = calculated_subtotal * 0.09
     calculated_total = calculated_subtotal + calculated_tax
-    # Save the order in the database
+
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       subtotal: calculated_subtotal,
       tax: calculated_tax,
       total: calculated_total,
     )
     @order.save
+    carted_products.update_all(status: "purchased", order_id: @order.id)
     render "show.json.jb"
   end
 
